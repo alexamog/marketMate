56882b6b4451e4539a1a9b23dba512907b334ecf
Alex Amog <76136244+alexamog@users.noreply.github.com>
2023-04-10 18:42:39 -0700

    Update app.py

--- Storage/app.py
+++ Storage/app.py
@@ -139,9 +139,9 @@ logger = logging.getLogger('basic')
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-# app.add_api('openapi.yaml', strict_validation=True, validate_responses=True)
-app.add_api("openapi.yaml", base_path="/storage",
-            strict_validation=True, validate_responses=True)
+app.add_api('openapi.yaml', strict_validation=True, validate_responses=True)
+# app.add_api("openapi.yaml", base_path="/storage",
+#             strict_validation=True, validate_responses=True)
 
 logger = logging.getLogger('basic')
 
